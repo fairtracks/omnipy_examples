a463f174628656d188b23cfef6659d783458c4c2
pavelvazquez <pavelva@uio.no>
2023-11-02 12:31:06 +0100

    Added minio test files for list of buckets and upload

---
+++ workshops/2023.10 Biohackaton 2023/object_upload.py
@@ -0,0 +1,19 @@
+import boto3
+
+
+def upload_to_bucket(file_path):
+    #session = boto3.Session(profile_name='minio')
+    session = boto3.Session()
+    s3 = session.resource(
+        service_name = 's3',
+        aws_access_key_id = 'accesskeyexample123',
+        aws_secret_access_key = 'secretkeyexample123',
+        endpoint_url='https://omnipy.fairtracks.sigma2.no')
+
+    bucket = s3.Bucket('testbucket')
+    bucket.upload_file(file_path, file_path.split('/')[-1])
+
+
+if __name__ == "__main__":
+    file_path = "./testfile.txt"
+    upload_to_bucket(file_path)
---
+++ workshops/2023.10 Biohackaton 2023/test_minio.py
@@ -0,0 +1,16 @@
+import boto3
+
+session = boto3.session.Session(profile_name='minio')
+
+s3_client = session.client(
+    service_name='s3',
+    endpoint_url='https://omnipy.fairtracks.sigma2.no',
+)
+
+buckets = s3_client.list_buckets()
+for bucket in buckets['Buckets']:
+    bucket_name = bucket['Name']
+    print(f"Bucket: {bucket_name}")
+    print(s3_client.list_objects(Bucket=bucket_name).get('Contents', []))
+    for obj in s3_client.list_objects(Bucket=bucket_name).get('Contents', []):
+        print(obj['Key'])
