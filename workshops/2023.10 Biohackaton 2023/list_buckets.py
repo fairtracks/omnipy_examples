6b935bab6163cb982db932e8e379095484b2010e
pavelvazquez <pavelva@uio.no>
2023-11-02 12:31:06 +0100

    Added minio test files for list of buckets and upload

---
+++ workshops/2023.10 Biohackaton 2023/list_buckets.py
@@ -0,0 +1,23 @@
+import boto3
+
+
+def list_buckets():
+    #session = boto3.session.Session(profile_name='minio')
+
+
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name = 's3',
+        aws_access_key_id = 'accesskeyexample123',
+        aws_secret_access_key = 'secretkeyexample123',
+        endpoint_url='https://omnipy.fairtracks.sigma2.no',
+        use_ssl=True)
+
+    response = s3.list_buckets()
+
+    return [bucket['Name'] for bucket in response['Buckets']]
+
+
+buckets = list_buckets()
+for bucket in buckets:
+    print(bucket)
