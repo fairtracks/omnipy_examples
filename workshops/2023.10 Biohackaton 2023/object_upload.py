2036d03035a0eaa25ac695e6a7337749f931ab8e
Sveinung Gundersen <sveinugu@gmail.com>
2024-01-04 07:31:01 +0100

    Lint fix and cleanup

--- workshops/2023.10 Biohackaton 2023/object_upload.py
+++ workshops/2023.10 Biohackaton 2023/object_upload.py
@@ -5,9 +5,9 @@ def upload_to_bucket(file_path):
     #session = boto3.Session(profile_name='minio')
     session = boto3.Session()
     s3 = session.resource(
-        service_name = 's3',
-        aws_access_key_id = 'accesskeyexample123',
-        aws_secret_access_key = 'secretkeyexample123',
+        service_name='s3',
+        aws_access_key_id='accesskeyexample123',
+        aws_secret_access_key='secretkeyexample123',
         endpoint_url='https://omnipy.fairtracks.sigma2.no')
 
     bucket = s3.Bucket('testbucket')
