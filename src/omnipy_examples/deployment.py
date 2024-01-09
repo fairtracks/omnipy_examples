105b0e38fd0a0d9b286ca83d8976154813251e0a
Sveinung Gundersen <sveinugu@gmail.com>
2024-01-09 23:07:12 +0100

    Set interactive_mode to False on prefect deployments

--- src/omnipy_examples/deployment.py
+++ src/omnipy_examples/deployment.py
@@ -2,42 +2,41 @@ from omnipy import runtime
 from omnipy_examples.main import chatgpt, dagsim, encode, gff, isajson, uniprot
 from prefect import flow as prefect_flow
 
+runtime.config.engine = 'prefect'
+runtime.config.data.interactive_mode = False
+
 
 @prefect_flow
 def dagsim_prefect():
-    runtime.config.engine = 'prefect'
     dagsim()
 
 
 @prefect_flow
 def encode_prefect():
-    runtime.config.engine = 'prefect'
     encode()
 
 
 @prefect_flow
 def gff_prefect():
-    runtime.config.engine = 'prefect'
     gff()
 
 
 @prefect_flow
 def isajson_prefect():
-    runtime.config.engine = 'prefect'
     isajson()
 
 
 @prefect_flow
 def uniprot_prefect():
-    runtime.config.engine = 'prefect'
     uniprot()
 
 
 @prefect_flow
 def chatgpt_prefect():
-    runtime.config.engine = 'prefect'
     chatgpt()
 
+
+uniprot_prefect()
 
 # isajson_prefect.deploy(
 #     'isajson', work_pool_name='kubernetes-agent', image='fairtracks/omnipy-examples:latest')
