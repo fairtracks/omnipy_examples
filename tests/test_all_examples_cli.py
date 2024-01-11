2e6c9f7850fa071b29b95cb3942944d5bc3ff63a
Sveinung Gundersen <sveinugu@gmail.com>
2024-01-11 12:46:55 +0100

    Lint fix

--- tests/test_all_examples_cli.py
+++ tests/test_all_examples_cli.py
@@ -15,6 +15,6 @@ TESTABLE_COMMANDS = [
 @pytest.mark.parametrize("command", TESTABLE_COMMANDS)
 def test_invoke_all_testable_examples(command: str):
     return_code = call(
-        f'python {Path(__file__).parent.parent.joinpath("src/omnipy_examples/main.py")} {command}',
+        f'python {Path(__file__).parent.parent.joinpath('src/omnipy_examples/main.py')} {command}',
         shell=True)
     assert return_code == 0
