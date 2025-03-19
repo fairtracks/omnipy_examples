a0dc1f9de1480b1daac84bba1cd847f5732e4ffc
Sveinung Gundersen <sveinugu@gmail.com>
2025-03-19 18:19:32 +0100

    Refactor get_github_repo_urls function to use HttpUrlModel and Dataset.load()

--- src/omnipy_examples/util.py
+++ src/omnipy_examples/util.py
@@ -1,13 +1,21 @@
-import httpx
-from omnipy import HttpUrlDataset, JsonModel, Model, TaskTemplate
-
+from omnipy import HttpUrlDataset, Model, TaskTemplate, HttpUrlModel, JsonListOfDictsDataset
 
 @TaskTemplate()
 def get_github_repo_urls(owner: str, repo: str, branch: str, path: str,
-                         file_suffix: str) -> HttpUrlDataset:
-    api_url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}?ref={branch}'
-    url_pre = f'https://raw.githubusercontent.com/{owner}/{repo}/{branch}/{path}'
+                         file_suffix: str|None = None) -> HttpUrlDataset:
 
-    json_data = JsonModel(httpx.get(api_url).raise_for_status().json())
-    names = Model[list[str]]([f['name'] for f in json_data if f['name'].endswith(file_suffix)])
-    return HttpUrlDataset({name: f'{url_pre}/{name}' for name in names})
+    url_pre = HttpUrlModel('https://raw.githubusercontent.com')
+    url_pre.path // owner // repo // branch // path
+
+    if file_suffix:
+        api_url = HttpUrlModel('https://api.github.com')
+        api_url.path // 'repos' // owner // repo // 'contents' // path
+        api_url['ref'] = branch
+
+        json_data = JsonListOfDictsDataset()
+        json_data.load(api_url)
+        names = Model[list[str]]([f['name'] for f in json_data[0] if f['name'].endswith(file_suffix)])
+        return HttpUrlDataset({name: f'{url_pre}/{name}' for name in names})
+    else:
+        name = url_pre.path.name
+        return HttpUrlDataset({name: f'{url_pre}/{name}'})
