535977508505cf2706f3bf1b227cabfd43703b32
Sveinung Gundersen <sveinugu@gmail.com>
2025-03-20 12:15:18 +0100

    Bugfixes in get_github_repo_urls()

--- src/omnipy_examples/util.py
+++ src/omnipy_examples/util.py
@@ -1,8 +1,14 @@
-from omnipy import HttpUrlDataset, Model, TaskTemplate, HttpUrlModel, JsonListOfDictsDataset
+from pathlib import Path
+
+from omnipy import HttpUrlDataset, HttpUrlModel, JsonListOfDictsDataset, Model, TaskTemplate
+
 
 @TaskTemplate()
-def get_github_repo_urls(owner: str, repo: str, branch: str, path: str,
-                         file_suffix: str|None = None) -> HttpUrlDataset:
+def get_github_repo_urls(owner: str,
+                         repo: str,
+                         branch: str,
+                         path: str | Path,
+                         file_suffix: str | None = None) -> HttpUrlDataset:
 
     url_pre = HttpUrlModel('https://raw.githubusercontent.com')
     url_pre.path // owner // repo // branch // path
@@ -10,12 +16,13 @@ def get_github_repo_urls(owner: str, repo: str, branch: str, path: str,
     if file_suffix:
         api_url = HttpUrlModel('https://api.github.com')
         api_url.path // 'repos' // owner // repo // 'contents' // path
-        api_url['ref'] = branch
+        api_url.query['ref'] = branch
 
         json_data = JsonListOfDictsDataset()
         json_data.load(api_url)
-        names = Model[list[str]]([f['name'] for f in json_data[0] if f['name'].endswith(file_suffix)])
+        names = Model[list[str]](
+            [f['name'] for f in json_data[0] if f['name'].endswith(file_suffix)])
         return HttpUrlDataset({name: f'{url_pre}/{name}' for name in names})
     else:
         name = url_pre.path.name
-        return HttpUrlDataset({name: f'{url_pre}/{name}'})
+        return HttpUrlDataset({name: url_pre})
