b27dcf0e1472ceb7c095fca25ce178561be9a023
Sveinung Gundersen <sveinugu@gmail.com>
2026-05-05 08:27:50 +0200

    Cleanup encode example by removing unused code

--- src/omnipy_examples/encode.py
+++ src/omnipy_examples/encode.py
@@ -5,40 +5,7 @@ from omnipy import (convert_dataset_list_of_dicts_to_pandas,
                     remove_columns)
 from omnipy.components._fairtracks.tasks import import_dataset_from_encode
 
-# cast_to_dict_on_top = cast_dataset.refine(
-#     name='cast_to_dict_on_top',
-#     fixed_params=dict(cast_model=JsonDictOfAnyModel),
-# )
-
-# encode_json_pruned =
-# encode_json_dict = cast_to_dict_on_top.run(encode_json)
-
-# runtime.config.engine = 'prefect'
-# runtime.config.prefect.use_cached_results = True
-#
-#
-# @FuncFlowTemplate()
-# def import_encode_data_tmpl():
-#     encode_json = import_dataset_from_encode(
-#         endpoints=[
-#             'experiment',
-#             'biosample',
-#         ],
-#         max_data_item_count=25,
-#         serialize_as='csv',
-#     )
-#     encode_json_pruned = remove_columns(
-#         encode_json,
-#         column_keys_for_data_items=dict(
-#             experiment=['audit'],
-#             biosample=['audit'],
-#         ),
-#         serialize_as='csv',
-#     )
-#     return encode_json_pruned
-#
-#
-# import_encode_data.run()
+# TODO: Reimplement encode example using Dataset.load()
 
 
 @LinearFlowTemplate(
